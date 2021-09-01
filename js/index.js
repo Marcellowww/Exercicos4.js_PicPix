9f5abb2564ee50f551fa87ef9c389ecc0543a0c8
Marcelo <guedesmarcello733@gmail.com>
2021-09-01 01:15:37 -0300

    Primeiro Commit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function process(){
+    console.log(bank.value)
+}
+
+function verify(){
+    const accountNumber = document.getElementById('account-number');
+    const bank = document.getElementById('bank');
+    const bankCode = document.getElementById('bank-code');
+    const name = document.getElementById('name');
+    const value =  document.getElementById('value');
+
+    if(accountNumber.value == "" | bank.value == "" | bankCode.value == "" | name.value == "" | value.value == "")return;
+        
+    process();
+    
+
+    
+}
+
+    
